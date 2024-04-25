d33802d49f965d288758a28d5e4c9b18545fede3
K <daystudio@gmail.com>
2024-04-25 23:39:06 +0800

    initial commit.

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LLM",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v4),
+        .tvOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "LLM",
+            targets: ["LLM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ggerganov/llama.cpp/", branch: "master"),
+        .package(url: "https://github.com/kishikawakatsumi/swift-power-assert", from: "0.12.0"),
+    ],
+    targets: [
+        .target(
+            name: "LLM",
+            dependencies: [
+                .product(name: "llama", package: "llama.cpp")
+            ]),
+        .testTarget(
+            name: "LLMTests",
+            dependencies: [
+                .product(name: "PowerAssert", package: "swift-power-assert"),
+                "LLM"
+            ]),
+    ]
+)
---
+++ Tests/LLMTests/LLMTests.swift
@@ -0,0 +1,231 @@
+import XCTest
+import PowerAssert
+@testable import LLM
+
+final class LLMTests: XCTestCase {
+    //MARK: Template tests
+    let systemPrompt = "You are a human."
+    let userPrompt = "Are you a human or an AI?"
+    let history = [Chat(.user, "Hey."), Chat(.bot, "Hi.")]
+    
+    func testChatMLPreProcessorWithoutSystemMessage() throws {
+        let template = Template.chatML()
+        let expected = """
+        <|im_start|>user
+        \(userPrompt)<|im_end|>
+        <|im_start|>assistant
+        
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testChatMLPreProcessorWithoutHistory() throws {
+        let template = Template.chatML(systemPrompt)
+        let expected = """
+        <|im_start|>system
+        \(systemPrompt)<|im_end|>
+        <|im_start|>user
+        \(userPrompt)<|im_end|>
+        <|im_start|>assistant
+        
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testChatMLPreProcessorWithHistory() throws {
+        let template = Template.chatML(systemPrompt)
+        let expected = """
+        <|im_start|>system
+        \(systemPrompt)<|im_end|>
+        <|im_start|>user
+        \(history[0].content)<|im_end|>
+        <|im_start|>assistant
+        \(history[1].content)<|im_end|>
+        <|im_start|>user
+        \(userPrompt)<|im_end|>
+        <|im_start|>assistant
+        
+        """
+        let output = template.preprocess(userPrompt, history)
+        #assert(expected == output)
+    }
+    
+    func testAlpacaPreProcessorWithoutSystemMessage() throws {
+        let template = Template.alpaca()
+        let expected = """
+        ### Instruction:
+        \(userPrompt)
+        
+        ### Response:
+        
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testAlpacaPreProcessorWithoutHistory() throws {
+        let template = Template.alpaca(systemPrompt)
+        let expected = """
+        \(systemPrompt)
+
+        ### Instruction:
+        \(userPrompt)
+        
+        ### Response:
+        
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testAlpacaPreProcessorWithHistory() throws {
+        let template = Template.alpaca(systemPrompt)
+        let expected = """
+        \(systemPrompt)
+
+        ### Instruction:
+        \(history[0].content)
+        
+        ### Response:
+        \(history[1].content)
+        
+        ### Instruction:
+        \(userPrompt)
+        
+        ### Response:
+        
+        """
+        let output = template.preprocess(userPrompt, history)
+        #assert(expected == output)
+    }
+    
+    func testLLaMaPreProcessorWithoutSystemMessage() throws {
+        let template = Template.llama()
+        let expected = """
+        <s>[INST] \(userPrompt) [/INST]
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testLLaMaPreProcessorWithoutHistory() throws {
+        let template = Template.llama(systemPrompt)
+        let expected = """
+        <s>[INST] <<SYS>>
+        \(systemPrompt)
+        <</SYS>>
+
+        \(userPrompt) [/INST]
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testLLaMaPreProcessorWithHistory() throws {
+        let template = Template.llama(systemPrompt)
+        let expected = """
+        <s>[INST] <<SYS>>
+        \(systemPrompt)
+        <</SYS>>
+
+        \(history[0].content) [/INST] \(history[1].content)</s><s>[INST] \(userPrompt) [/INST]
+        """
+        let output = template.preprocess(userPrompt, history)
+        #assert(expected == output)
+    }
+    
+    func testMistralPreProcessorWithoutHistory() throws {
+        let template = Template.mistral
+        let expected = """
+        <s>[INST] \(userPrompt) [/INST]
+        """
+        let output = template.preprocess(userPrompt, [])
+        #assert(expected == output)
+    }
+    
+    func testMistralPreProcessorWithHistory() throws {
+        let template = Template.mistral
+        let expected = """
+        <s>[INST] \(history[0].content) [/INST]\(history[1].content)</s> [INST] \(userPrompt) [/INST]
+        """
+        let output = template.preprocess(userPrompt, history)
+        #assert(expected == output)
+    }
+    
+    //MARK: HuggingFaceModel tests
+    lazy var model = HuggingFaceModel("TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF", .Q2_K, template: .chatML(systemPrompt))
+    let urlString = "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q2_K.gguf?download=true"
+    
+    func testRegexMatchCaseInsensitivity() async throws {
+        let hasMatch = try! #"(?i)Q2_K"#.hasMatch(in: urlString.lowercased())
+        let expected = true
+        #assert(hasMatch == expected)
+    }
+    
+    func testFilterHasMatch() async throws {
+        let hasMatch = try! model.filterRegexPattern.hasMatch(in: urlString)
+        let expected = true
+        #assert(hasMatch == expected)
+    }
+    
+    func testGetDownloadURLStringsFromHuggingFaceModel() async throws {
+        let urls = try await model.getDownloadURLStrings()
+        let expected = [
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q2_K.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q3_K_L.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q3_K_M.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q3_K_S.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_0.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_S.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q5_0.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q5_K_M.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q5_K_S.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q6_K.gguf?download=true",
+            "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q8_0.gguf?download=true"
+        ]
+        #assert(urls == expected)
+    }
+    
+    func testGetDownloadURLFromHuggingFaceModel() async throws {
+        let url = try await model.getDownloadURL()!
+        let expected = URL(string: urlString)!
+        #assert(url == expected)
+    }
+    
+    func testInitFromHuggingFaceModel() async throws {
+        let bot = try await LLM(from: model)
+        #assert(!bot.path.isEmpty)
+    }
+    
+    func testInitializerWithTempate() async throws {
+        let template = model.template
+        let bot = try await LLM(from: model)
+        #assert(bot.preprocess(userPrompt, []) == template.preprocess(userPrompt, []))
+    }
+    
+    func testInferenceFromHuggingFaceModel() async throws {
+        let bot = try await LLM(from: model)
+        let input = "have you heard of this so-called LLM.swift library?"
+        await bot.respond(to: input)
+        #assert(!bot.output.isEmpty)
+    }
+    
+    func testRecoveryFromLengtyInput() async throws {
+        let bot = try await LLM(from: model, maxTokenCount: 16)
+        let input = "have you heard of this so-called LLM.swift library?"
+        await bot.respond(to: input)
+        #assert(bot.output == "tl;dr")
+    }
+    
+    func testEncodingDecodingFromHuggingFaceModel() async throws {
+        let bot = try await LLM(from: model)
+        let input = "have you heard of this so-called LLM.swift library?"
+        let tokens = bot.encode(input)
+        let decoded = bot.decode(tokens).trimmingCharacters(in: .whitespacesAndNewlines)
+        #assert(!tokens.isEmpty)
+        #assert(decoded == input)
+    }
+}
