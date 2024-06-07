b3e886a08cd4272bb1e5460c4b6a56af3860155a
K <daystudio@gmail.com>
2024-06-07 21:32:06 +0800

    updated llama3.

--- Sources/LLM/LLM.swift
+++ Sources/LLM/LLM.swift
@@ -577,7 +577,7 @@ public struct Template {
             system: ("<|start_header_id|>system<|end_header_id|>\n\n", "<|eot_id|>"),
             user: ("<|start_header_id|>user<|end_header_id|>\n\n", "<|eot_id|>"),
             bot: ("<|start_header_id|>assistant<|end_header_id|>\n\n", "<|eot_id|>"),
-            stopSequence: "<|end_of_text|>",
+            stopSequence: "\n\n",
             systemPrompt: systemPrompt,
             shouldDropLast: true
         )
